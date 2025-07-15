4efca0562b08446fa208f5df3c5bccee2136a322
La svedese <5554930+sarasjodin@users.noreply.github.com>
2025-07-15 10:22:53 +0200

    eslit update

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,43 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import parser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
-]);
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        document: true,
+        window: true,
+        console: true,
+        localStorage: true,
+        fetch: true,
+        setTimeout: true,
+        clearTimeout: true,
+        location: true,
+        HTMLElement: true,
+        HTMLFormElement: true,
+        HTMLInputElement: true,
+        HTMLSelectElement: true,
+        HTMLButtonElement: true,
+        MouseEvent: true,
+        SubmitEvent: true,
+        confirm: true,
+        Response: true,
+        MutationObserver: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+  js.configs.recommended,
+];
